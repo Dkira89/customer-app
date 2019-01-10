c7819795fa2e1593297e88d1b9aa8cc6a4dc5a91
crispis <cristianzuletaisc@gmail.com>
2019-01-10 16:15:00 -0500

    Se implementa redux-forms al proyectro

--- src/App.js
+++ src/App.js
@@ -1,18 +1,33 @@
 import React, { Component } from 'react';
-import {BrowserRouter as Router,Route} from 'react-router-dom';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import './App.css';
 import './index.css';
 import HomeContainer from './containers/HomeContainer';
-import Rutas  from './Routes';
+import CustomersContainer from './containers/CustomersContainer';
+import CustomerContainer from './containers/CustomerContainer';
 
 class App extends Component {
+  renderHome = () =><HomeContainer></HomeContainer>
+  renderCustomerContainer = () => <CustomersContainer></CustomersContainer>
   renderCustomerListContainer = () => <h1>Customer List Container</h1>
   renderCustomerNewContainer = () => <h1>Customer New Container</h1>
 
   render() {
     return (
       <Router>
-          <Rutas></Rutas>
+        <div>
+          <Route exact path="/" component = {this.renderHome}></Route>
+          <Route exact path="/customers" component = {this.renderCustomerContainer}></Route>
+          <Switch>
+            <Route exact path="/customers/new" component = {this.renderCustomerNewContainer}></Route>
+            <Route exact path="/customers/:dni/edit" 
+                   render = {props=><CustomerContainer dni={props.match.params.dni}></CustomerContainer>}>
+            </Route>
+            <Route exact path="/customers/:dni" 
+                   render = {props=><CustomerContainer dni={props.match.params.dni}></CustomerContainer>}>
+            </Route>
+          </Switch>
+        </div>
       </Router>
       
     );
